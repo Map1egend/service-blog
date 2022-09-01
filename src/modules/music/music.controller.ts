3d57aeb1a9749f6f82ef8905778b1cc225d12a00
Maple <jellyfishtatta@icloud.com>
2022-09-01 09:57:01 +0800

    fix: song/url 错误处理

--- src/modules/music/music.controller.ts
+++ src/modules/music/music.controller.ts
@@ -1,34 +1,82 @@
-import { Controller, Get, Post, Query, UploadedFiles, UseInterceptors, Response, Body } from '@nestjs/common'
-import { FilesInterceptor } from '@nestjs/platform-express'
+import { Controller, Get, Post, Query, UploadedFiles, UseInterceptors, Response } from '@nestjs/common'
+import { FileFieldsInterceptor } from '@nestjs/platform-express'
 import { join } from 'path'
 import * as fs from 'fs'
 
 @Controller('music')
 export class MusicContoller {
-  @Get('song/url/v1')
-  findMusicUrl(@Query() query): string {
-    return `Query 参数是: ${query.id}${query.level}`
+  @Get('song/url')
+  findMusicUrl(@Query() query, @Response() res) {
+    const dirPath = join(__dirname, '..', '..', '..', 'public', 'daily')
+    if (fs.existsSync(`${dirPath}/${query.id}.json`)) {
+      const readUrl = fs.createReadStream(`${dirPath}/${query.id}.json`)
+  
+      let arr =[]
+      readUrl.on('data', function (chunk) {
+        arr.push(chunk)
+      })
+      readUrl.on('end', function (chunk) {
+        res.send({
+          status: 'success',
+          message: JSON.parse(Buffer.concat(arr).toString())
+        })
+      })
+      readUrl.on('err', function (err) {
+        console.log(err)
+      })
+    } else {
+      res.send({
+        status: 'error',
+        message: '未发布'
+      })
+    }
   }
 
   @Post('song/upload')
-  @UseInterceptors(FilesInterceptor('files'))
-  upload(@UploadedFiles() files: Array<Express.Multer.File>, @Response() res) {
-    console.log(files)
+  @UseInterceptors(FileFieldsInterceptor([
+    { name: 'cover', maxCount: 1 },
+    { name: 'bgi', maxCount: 1 },
+    { name: 'audio', maxCount: 1 },
+    { name: 'lyric', maxCount: 1 }
+  ]))
+  upload(@UploadedFiles() files: { cover?: Express.Multer.File[], bgi?: Express.Multer.File[], audio?: Express.Multer.File[], lyric?: Express.Multer.File[] }, @Response() res) {
     //创建文件夹目录
-    const dirPath = join(__dirname, '..', '..', '..', 'public', 'daily', String(+new Date))
+    let publishTime = new Date()
+    publishTime.setHours(8)
+    publishTime.setMinutes(0)
+    publishTime.setSeconds(0)
+    publishTime.setMilliseconds(0)
+    const dirPath = join(__dirname, '..', '..', '..', 'public', 'daily', String(+publishTime))
 
     if (!fs.existsSync(dirPath)) {
-      fs.mkdirSync(dirPath);
-      console.log("文件夹创建成功");
+      fs.mkdirSync(dirPath)
+      console.log("文件夹创建成功")
     } else {
-      console.log("文件夹已存在");
+      let files = fs.readdirSync(dirPath)
+      files.forEach(file => {
+        const filePath = dirPath + '/' + file
+        fs.unlinkSync(filePath)
+      })
+      console.log("文件夹已存在, 清空完毕")
     }
 
-    files.forEach(file => {
-      let newPath = dirPath + "/" + file.originalname
-      const writeImage = fs.createWriteStream(newPath)
-      writeImage.write(file.buffer)
-    })
+    const readurl = 'http://127.0.0.1:3000/daily'
+    let urlJson = {}
+
+    for(let key in files) {
+      const filename = files[key][0].originalname.replace(/.+\./, key + '.')
+      let newPath = dirPath + "/" + filename
+      if (key !== 'lyric') {
+        const writeStream = fs.createWriteStream(newPath)
+        writeStream.write(files[key][0].buffer)
+        urlJson[key] = `${readurl}/${+publishTime}/${filename}`
+      } else {
+        console.log(JSON.parse(files[key][0].buffer.toString()))
+        urlJson[key] = JSON.parse(files[key][0].buffer.toString()).lyric
+      }
+    }
+    const writeImage = fs.createWriteStream(join(dirPath, '..', `${+publishTime}.json`))
+    writeImage.write(JSON.stringify(urlJson))
     res.send({ data: "上传成功！" })
   }
 }
